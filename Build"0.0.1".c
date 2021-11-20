f0b5f4da69ee89c4227a881105820183c0f4f2a1
Junus-programmer <76218656+Junus-programmer@users.noreply.github.com>
2021-11-20 14:13:07 +0100

    Create Build"0.0.1".c

---
+++ Build"0.0.1".c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+
+    while(1){
+        animate("<o>", ">o<");
+    }
+
+
+    return 0;
+}
+
+void animate(char pix1[10], char pix2[10])
+{
+    
+    printf("%s", pix1);
+    printf("%s", pix2);
+    
+    
+    
+}
